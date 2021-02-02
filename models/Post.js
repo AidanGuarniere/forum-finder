75603a48ee558e2d8f275247f3422663be7a95cb
aidan <aidanguarniere@gmail.com>
2021-02-02 14:50:27 -0800

    defined Post columns

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+// requirements
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create Post model
+class Post extends Model {}
+
+// define Post columns
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
