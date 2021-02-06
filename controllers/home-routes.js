f0b52b5c54a49c5fc99e63a925054c9484734bb9
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-02-05 23:10:35 -0800

    Feat: Base work put into the home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,76 +1,106 @@
-// const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const { Favorite, Forum, Post, User, Vote} = require('../models')
+// This file will contain all of the user-facing routes, such as the homepage and login page.
 
-// // We'll want to display our forums to the homepage
-// // I believe the code should go in this order:
-// // Forums -> Include Display Posts within the Forum -> Include User who created Post
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Favorite, Forum, Post, User, Vote} = require('../models');
 
-// /*
-// EDIT: 
-// Since this is the home route we should likely display just the forums available
-// Anything pertaining to the post or the user should be left for the FORUM-ROUTES instead.
-// However what we can include is amount of posts in forum
-// */
+/* 
+When in the home-page, we'll want to find all available forums.
+Need these forums to display their title. the amount of posts, number of votes
+And if it's been favorited or not. So let's include those when we Forum.findAll
+*/
 
-// // When in the homepage find all the available forums
-// router.get('/', (req, res) => {
-//     Forum.findAll({
-//         attributes: [ 'id', 'title', 'initial_message', 'user_id' ],
-//         // Include the post model, can possibly reference how many posts are within forum
-//         include: [
-//             {
-//                 model: Post,
-//                 attributes: [ 'id', 'post_text', 'user_id', 'post_id' ],
-//             },
-//         ]
-//     })
-//     .then(dbForumsData => {
-//         const forums = dbForumsData.map(forum => forum.get({ plain: true }));
-
-//         res.render('homepage', {
-//             forums,
-//             loggedIn: req.session.loggedIn
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
-
-// /* 
-// If looking for a specific forum, find that.
-// If the user's at a specific forum, 
-// then present all the posts within this forum
-// */
-// router.get('/forums/:id', (req, res) => {
-//     Forum.findOne({
-//         where: {
-//             id: req.params.id
-//         },
-//         attributes: [ 'id', 'title', 'initial_message', 'user_id' ],
-//         include: [
-//             {
-//                 model: Post,
-//                 attributes: [ 'id', 'post_text', 'user_id', 'post_id' ],
-//             }
-//         ]
-//     })
-//     .then(dbForumsData => {
+// At the homepage find all the available forums
+router.get('/', (req, res) => {
+    Forum.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        // Include the post model, can possibly reference how many posts are within forum
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'post_text', 'user_id', 'post_id', 'created_at'],
+                // A post should include the username
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbForumData => {
+        // Need the entire array of posts to be in the template. 
+        // That also means we'll need to serialize the entire array.
+        // This will loop over and map each Sequelize object into a serialized version of itself, saving the results in a new forums array.
+        const forums = dbForumData.map(forum => forum.get({ plain: true }));
         
-//         // If the Forum doesn't exist, send error to user
-//         if (!dbForumsData) {
-//             res.status(404).json({ message: 'This forum does not exist' });
-//             return;
-//         }
+        // Pass this data to the template
+        res.render('homepage', { forums });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-//         // We'll need to serialize this data
-//         // THEN pass that data to the template (WHEN READY)
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// })
-// module.exports = router;
+// If looking for a specific forum, find that. 
+// If the user's at a specific forum, then present all the posts within this forum
+router.get('/forums/:id', (req, res) => {
+    Forum.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+            include: [
+            {
+                model: Post,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbForumData => {
+        // If the Forum doesn't exist, send error to user
+        if (!dbForumData) {
+            res.status(404).json({ message: 'No forum with this id found' });
+            return;
+        }
+
+        // Serialize this data
+        const forum = dbForumData.get({ plain: true });
+
+        // Pass this data to the template
+        res.render('singel-forum', { forum });
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Route that renders login
+// Login page doesn't need any variables, so we don't need to pass a second argument to the render() method.
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+module.exports = router;
