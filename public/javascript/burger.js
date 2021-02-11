d017fa0a20c284099bb40c6acb79483e362686c2
Vanessa G <vlopez_jv@hotmail.com>
2021-02-11 14:01:20 -0800

    new working branch

--- public/javascript/burger.js
+++
@@ -1,6 +0,0 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
-});
