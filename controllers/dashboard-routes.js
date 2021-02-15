525f0164c314ce08a7a02cc3ade2dd7a9f9c69ac
Vanessa G <vlopez_jv@hotmail.com>
2021-02-14 23:35:21 -0800

    deployed dashboard, modified code & tbd

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,118 +1,132 @@
+
+
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Favorite, Forum, Post, User, Vote } = require("../models/");
 
-// The dashboard should only display posts created by the logged in user
-router.get("/", (req, res) => {
-  Forum.findAll({
-    where: {
-      // use the ID from the session
-      user_id: req.body.user_id,
-    },
-    attributes: [
-      "id",
-      "title",
-      "initial_message",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM favorite WHERE forum.id = favorite.forum_id)"
-        ),
-        "favorite_count",
-      ],
-    ],
-    include: [
-      {
-        model: Post,
-        attributes: [
-          "id",
-          "post_text",
-          "forum_id",
-          "user_id",
-          "created_at",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count",
-          ],
-        ],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-  .then((dbForumData) => {
-    // serialize data before passing to template
-    const forums = dbForumData.map((forum) => forum.get({ plain: true }));
-    res.render("/dashboard", { forums });
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
-
-// Update the API endpoints to use the id from the session
-router.get("/edit/id", (req, res) => {
-  Forum.findByPk(req.params.id, {
-    attributes: [
-      "id",
-      "title",
-      "initial_message",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM favorite WHERE forum.id = favorite.forum_id)"
-        ),
-        "favorite_count",
-      ],
-    ],
-    include: [
-      {
-        model: Post,
-        attributes: [
-          "id",
-          "post_text",
-          "forum_id",
-          "user_id",
-          "created_at",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count",
-          ],
-        ],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbForumData) => {
-      if (dbForumData) {
-        const forum = dbForumData.get({ plain: true });
-
-        res.render("forum-edit", { forum });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+router.get('/', (req, res) => {
+  res.render('dashboard', {loggedIn: true });
 });
 
 module.exports = router;
+
+// // The dashboard should only display posts created by the logged in user
+// router.get("/", (req, res) => {
+//   Forum.findAll({
+//     where: {
+//       // use the ID from the session
+//       user_id: req.session.user_id,
+//     },
+//     attributes: [
+//       "id",
+//       "title",
+//       //"initial_message",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM favorite WHERE forum.id = favorite.forum_id)"
+//         ),
+//         "favorite_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Post,
+//         attributes: [
+//           "id",
+//           "post_text",
+//           "forum_id",
+//           "user_id",
+//           "created_at",
+//           [
+//             sequelize.literal(
+//               "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//             ),
+//             "vote_count",
+//           ],
+//         ],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//   .then((dbForumData) => {
+//     // serialize data before passing to template
+//     const forums = dbForumData.map(forum => forum.get({ plain: true}));
+//     res.render('dashboard', { forums, loggedIn: true });
+//   })
+//   //   const forums = dbForumData.map((forum) => forum.get({ plain: true }));
+//   //   res.render("/dashboard", {
+//   //     forums,
+//   //     loggedIn: req.session.loggedIn
+//   //   });
+//   // })
+//   .catch((err) => {
+//     console.log(err);
+//     res.status(500).json(err);
+//   });
+// });
+
+// // Update the API endpoints to use the id from the session
+// router.get("/edit/id", (req, res) => {
+//   Forum.findByPk(req.params.id, {
+//     attributes: [
+//       "id",
+//       "title",
+//       // "initial_message",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM favorite WHERE forum.id = favorite.forum_id)"
+//         ),
+//         "favorite_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Post,
+//         attributes: [
+//           "id",
+//           "post_text",
+//           "forum_id",
+//           "user_id",
+//           "created_at",
+//           [
+//             sequelize.literal(
+//               "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//             ),
+//             "vote_count",
+//           ],
+//         ],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbForumData) => {
+//       if (dbForumData) {
+//         const forum = dbForumData.get({ plain: true });
+
+//         res.render("forum-edit", { forum });
+//       } else {
+//         res.status(404).end();
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     });
+// });
+
+// module.exports = router;
